8b97574c0db9b8ef02d8c563fe4cc4831f1f283b
NEDONION <nedjiachenghu@gmail.com>
2023-04-08 04:10:40 -0500

    chore: configs

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,9 +1,27 @@
 import type { NextPage } from 'next';
+import { Container } from 'react-bootstrap';
+import React from 'react';
 
-const Footer: NextPage = () => {
+// const Footer: NextPage = () => {
+//     return (
+//         <div>这是页脚</div>
+//     )
+// }
+//
+// export default Footer;
+
+const Index = () => {
+    const siteName = "ned-test";
+    const cc = `${siteName}`;
     return (
-        <div>这是页脚</div>
-    )
-}
+      <footer className="bg-light py-3">
+          <Container>
+              <p className="text-center mb-0 fs-14 text-secondary">
+                      Built on NEDONION
+              </p>
+          </Container>
+      </footer>
+    );
+};
 
-export default Footer;
+export default React.memo(Index);
